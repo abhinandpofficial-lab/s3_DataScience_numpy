ed32783b7a6a488aedb3310c825b51fa7d932909
abhinandpofficial-lab <abhinandpofficial@gmail.com>
2026-09-01 15:41:19 +0530

    Initial commit

---
+++ program 8.py
@@ -0,0 +1,8 @@
+import numpy as np
+x=np.arange(16).reshape(4,4)
+print("Array:")
+print(x)
+header='C1 C2 C3 C4'
+np.savetxt('array.txt',x,fmt="%d",header=header)
+print("\nAfter loading,content of the file:")
+print(np.loadtxt('array.txt'))
